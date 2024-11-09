b5b73fa669ee8b400afde7a6e62c68dc4aecf858
voiteshonok <vsvoiteshonok1@gmail.com>
2024-11-09 20:02:58 +0400

    HW-3: added data fetching

--- src/components/CardCollection/CardCollection.jsx
+++ src/components/CardCollection/CardCollection.jsx
@@ -5,16 +5,47 @@ import { Card } from '../index'
 
 
 class CardCollection extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cards: [],
+            counter: 6
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://65de35f3dccfcd562f5691bb.mockapi.io/api/v1/meals')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ cards: data });
+            })
+            .catch(error => console.error('Error fetching data:', error));
+    }
+
+    handleSeeMore = () => {
+        this.setState((prevState) => ({
+            counter: prevState.counter + 6
+        }));
+    }
+
     render() {
-        const { cards } = this.props;
+        const { cards, counter } = this.state;
+        const visibleCards = cards.slice(0, counter);
+
         return (
             <div className='mainMenu'>
                 <div className='cardCollection'>
-                    {cards.map((card) => (
-                        <Card key={card.id} title={card.title} img={card.img} price={card.price} description={card.description}/>
+                    {visibleCards.map((card) => (
+                        <Card 
+                            key={card.id}
+                            title={card.meal}
+                            img={card.img}
+                            price={`$ ${card.price} USD`}
+                            description={card.category}
+                        />
                     ))}
                 </div>
-                <button className='mainMenu__more'>See more</button>
+                <button className='mainMenu__more'onClick={this.handleSeeMore}>See more</button>
             </div>
         )
     }
